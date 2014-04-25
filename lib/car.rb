1009bb1bf9f37151532c2a0234c95d75dd4a5b17
Kathleen Votolato <kvotolato3@gmail.com>
2014-04-24 22:48:50 -0400

    Add class variable to generate car id within the Car class

--- lib/car.rb
+++ lib/car.rb
@@ -1,12 +1,29 @@
 
 class Car
   USEFUL_LIFE = 10
+
+  # class variable
+  @car_id = 0
+
+# class method
+# getter for a class variable
+  def self.car_id
+    @car_id
+  end
+
+  def self.gen_car_id
+    @car_id += 1
+  end
+
+  attr_reader :car_id
+
   def initialize(init_value, make, model, year, dep_rate = 1.00/USEFUL_LIFE)
     @init_value = init_value
     @make = make
     @model = model
     @year = year
     @dep_rate = dep_rate
+    @car_id = Car.gen_car_id
   end
 end
 
